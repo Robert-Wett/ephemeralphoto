feb277166e6ae4d1447989cf81702e3101fe3767
Robert-Wett <rdwettlaufer@gmail.com>
2014-04-22 12:24:38 -0400

    Fixing routes/bodyparser

--- app.js
+++ app.js
@@ -3,7 +3,6 @@
  */
 
 var express = require('express')
-  , routes  = require('./routes')
   , http    = require('http')
   , path    = require('path')
   , config  = require('config');
@@ -16,23 +15,28 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.use(express.favicon());
 app.use(express.logger('dev'));
-app.use(express.bodyParser());
-app.use(express.methodOverride());
-app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.bodyParser({
+    uploadDir: '/uploads',
+    keepExtensions: true
+}));
 // http://goo.gl/XTvmi0
 // Limit filesizes to 5mb. This should take care of almost
 // all images, and cut down on people abusing the service to
 // host larger files. 
-app.use(express.limit(config.maxSize));
+app.use(express.limit(config.maxSize))
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'uploads')));
+;
 
 // development only
 if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-app.get('/',      routes.index);
-app.get('/image', routes.image);
+// Add the routes
+require('./routes')(app);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
