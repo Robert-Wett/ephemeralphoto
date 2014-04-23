4341561e90fbc5dd536b52599d8914e4f4314b02
Robert-Wett <rdwettlaufer@gmail.com>
2014-04-23 12:56:46 -0400

    Adding more defaults

--- config.js
+++ config.js
@@ -1,5 +1,11 @@
 module.exports = {
     filePath: '../base',
     maxSize: '5mb',
-    projectDir: __dirname
+    projectDir: __dirname,
+    defaultTtl: {
+        hours: 10
+    },
+    testTtl: {
+        seconds: 45
+    }
 };
