af062a9cd52b3934a655590c1adee1822f4b0e47
Robert Wettlaufer <rdwettlaufer@gmail.com>
2014-04-28 22:13:05 -0400

    fixing queued delete job

--- routes/image.js
+++ routes/image.js
@@ -6,89 +6,71 @@ var config      = require('../config');
 var uuid        = require('node-uuid');
 var _           = require('underscore');
 
-var usedFileNames = [];
-var fileLookup    = {};
 var contentPath   = config.projectDir + '/uploads/';
-var inDev         = process.env.dev === 'development';
+// var inDev         = process.env.dev === 'development';
 
 
 
-function queueDeleteJob(data, imageId) {
+var queueDeleteJob = function(data, imageId) {
   // Build the date object to pass into the Cron
   var ttlDate = new Date();
-  var seconds = data.seconds     || 0;
-  var minutes = data.minutes     || 1;
-  var hours   = data.hours       || 0;
-  var days    = data.days        || 0;
+  var ttlPeriod = data.seconds   || 0                 +
+                  ((data.minutes || 0) * 60)          +
+                  ((data.hours   || 0) * 60 * 60)     +
+                  ((data.days    || 0) * 24 * 60 * 60);
 
-  ttlDate.setSeconds(ttlDate.getSeconds() + seconds);
-  ttlDate.setMinutes(ttlDate.getMinutes() + minutes);
-  ttlDate.setHours(ttlDate.getHours() + hours + (days * 24));
+  if (ttlPeriod === 0) {
+    // Set to 15 seconds for dev
+    ttlPeriod = 15;
+  }
 
-  var cronJob = new CronJob(ttlDate, deleteImage(imageId), onJobFinish(imageId), true);
-  console.log(cronJob);
+
+  ttlDate.setSeconds(ttlDate.getSeconds() + ttlPeriod);
+  console.log(util.format("Setting the destruct time of '%s' to '%s' seconds", imageId, ttlPeriod));
+
+  var job = new CronJob({
+    cronTime: ttlDate,
+    onTick: function() {
+      var filePath = contentPath + imageId;
+      var outputString;
+
+      fs.unlink(filePath, function(err) {
+        if (err) {
+          outputString = "Error - unable to unlink file with id %s, with path %s";
+          console.log(util.format(outputString, imageId, filePath));
+        } else {
+          outputString = "Deleted file with id %s, with path %s!";
+          console.log(util.format(outputString, imageId, filePath));
+        }
+      });
+    },
+    onComplete: function() {
+      var outputString = "Successfully deleted %s at %s";
+      console.log(util.format(outputString, fileName, ttlDate));
+    },
+    start: false
+  });
+  job.start();
 
   // Return the Expiration Date for the front-end countdown
-  return ttlDate;
-}
-
-
-/**
- * [deleteImage description]
- * @param  {[type]} imgId [description]
- * @return {[type]}       [description]
- */
-function deleteImage(imgId) {
-  // If nothing passed, return
-  if (!imgId) return;
-
-  var filepath = contentPath + imgId;
-  var outputString;
-
-  fs.unlink(filepath, function(err) {
-    if (err) {
-      outputString = 'Error - unable to unlink file with id "%s", with path "%s"';
-      console.log(util.format(outputString, imgId, filePath));
-    } else {
-      usedFileNames.splice(usedFileNames.indexOf(imgId), 1);
-    }
-  });
-}
-
-
-function onJobFinish(fileName) {
-  if (inDev) {
-    var outputString = "Successfully deleted %s at %d";
-    console.log(util.format(outputString, fileName, Date.now));
-  }
-}
-
-
-/**
- * Get a guaranteed unique id checked against `usedFileNames`
- * @return {[String]} A unique identifier
- */
-function getGuid() {
-  var guid =  uuid.v1();
-  if (_.contains(usedFileNames, guid)) {
-    return getGuid();
-  }
-
-  usedFileNames.push(guid);
-  return guid;
-}
+  // return ttlDate;
+};
 
 
 module.exports = {
 
   showImage: function(req, res) {
     var fileName = req.params.id;
-    var img = fs.readFileSync(contentPath + fileName);
-    res.writeHead(200, {'Content-Type': 'image/jpg' });
-    res.end(img, 'binary');
+    fs.readFile(contentPath + fileName, function(err, img) {
+      if (err) next(err);
+
+      res.writeHead(200, {'Content-Type': 'image/jpg' });
+      res.end(img, 'binary');
+    });
   },
 
   postImage: function(req, res) {
+    console.log(req.body);
     fs.readFile(req.files.image.path, function (err, data) {
       var imageName = req.files.image.name;
 
@@ -99,8 +81,10 @@ module.exports = {
       }
       else {
         fs.writeFile(contentPath + imageName, data, function (err) {
+          if (err) next(err);
+
+          queueDeleteJob(req.body, imageName);
           res.redirect("/image/" + imageName);
-          //var ttlDate = queueDeleteJob(req.body, imageName);
           /*
           res.redirect('image', {
             image: imageName,
@@ -113,3 +97,25 @@ module.exports = {
   }
 
 };
+
+/*
+                                   __
+  __  ______  __  __________  ____/ /
+ / / / / __ \/ / / / ___/ _ \/ __  / 
+/ /_/ / / / / /_/ (__  )  __/ /_/ /  
+\__,_/_/ /_/\__,_/____/\___/\__,_/   
+                                     
+var usedFileNames = [];
+var fileLookup    = {};
+
+function getGuid() {
+  var guid =  uuid.v1();
+  if (_.contains(usedFileNames, guid)) {
+    return getGuid();
+  }
+
+  usedFileNames.push(guid);
+  return guid;
+}
+
+ */
